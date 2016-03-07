680abb8d180b8c7a9b9c9eb7233d1b49d6bdd9f1
Enrique Mendez <enrique.krft@gmail.com>
2016-03-06 21:02:53 -1000

    Make it print top folders

--- question.py
+++ question.py
@@ -1,1 +1,13 @@
+import os
 
+walker = os.walk('.')
+
+toptuple = walker.next()
+
+chapters = toptuple[1]	#get all the main problem types,
+
+#but we need to remove hidden folders.
+chapters = [x for x in chapters if x[0] != '.']
+print chapters
+
+# for eachchapter in chapters:
